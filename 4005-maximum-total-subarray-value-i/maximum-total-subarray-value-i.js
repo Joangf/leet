f922c8dd040400604cf956004779b77889473bcd
Hoàng Nguyễn <nguyenhoang1362005@gmail.com>
2026-06-09 20:04:22 +0700

    Time: 4 ms (58.33%) | Memory: 66.7 MB (83.33%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.js
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 var maxTotalValue = function(nums, k) {
-    const sortedNums = nums.toSorted((a, b) => a - b);
-    return (sortedNums[nums.length - 1] - sortedNums[0]) * k;
+    const max = Math.max(...nums);
+    const min = Math.min(...nums);
+    return (max - min) * k;
 };
