defab6e18bc00f03e823501c0674b0bdbf206356
Hoàng Nguyễn <nguyenhoang1362005@gmail.com>
2026-06-13 20:14:48 +0700

    Time: 76 ms (8.42%) | Memory: 64.6 MB (5.26%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {number[]} weights
+ * @return {string}
+ */
+var mapWordWeights = function (words, weights) {
+    let result = '';
+    for (const word of words) {
+        let sum = 0;
+        for (let i = 0; i < word.length; i++) {
+            const index = word[i].charCodeAt(0) - 97;
+            sum += weights[index];
+        }
+        console.log(String.fromCharCode(122 - (sum % 26)));
+        result += (String.fromCharCode(122 - (sum % 26)));
+    }
+    return result;
+};
